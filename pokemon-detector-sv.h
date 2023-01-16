0d092d15943c236251d2b87fd34b391245f515a9
Kaito Udagawa <umireon@gmail.com>
2023-01-16 19:46:23 +0900

    Update pokemon-detector-sv.h

--- pokemon-detector-sv.h
+++ pokemon-detector-sv.h
@@ -5,13 +5,24 @@ extern "C" {
 #endif
 
 struct pokemon_detector_sv_config {
-  const int screen_width = 1920;
-  const int screen_height = 1080;
-  const int num_of_max_opponent_pokemons = 6;
-  const int opponent_col_range[2]{1239, 1337};
-  const int opponent_row_range[6][2]{{228, 326}, {330, 428}, {432, 530},
-                                     {534, 632}, {636, 734}, {738, 836}};
+  const int screen_width;
+  const int screen_height;
+  const int num_of_max_opponent_pokemons;
+  const int opponent_col_range[2];
+  const int opponent_row_range[6][2];
 };
+
+const struct pokemon_detector_sv_config pokemon_detector_sv_default_config = {
+    .screen_width = 1920,
+    .screen_height = 1080,
+    .num_of_max_opponent_pokemons = 6,
+    .opponent_col_range = {1239, 1337},
+    .opponent_row_range = {{228, 326},
+                           {330, 428},
+                           {432, 530},
+                           {534, 632},
+                           {636, 734},
+                           {738, 836}}};
 
 struct pokemon_detector_sv_context;
 
