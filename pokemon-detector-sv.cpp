ea63e92321377111b403212d8d4dd3054b5cda01
Kaito Udagawa <umireon@gmail.com>
2023-01-21 01:56:57 +0900

    Mkdir -p before write files (#10)
    * a
    
    * Update pokemon-detector-sv.cpp
    
    * Update pokemon-detector-sv.cpp

--- pokemon-detector-sv.cpp
+++ pokemon-detector-sv.cpp
@@ -1,3 +1,4 @@
+#include <filesystem>
 #include <fstream>
 
 #include <opencv2/opencv.hpp>
@@ -10,154 +11,188 @@
 #include "SceneDetector.h"
 #include "SelectionRecognizer.h"
 
-static constexpr std::array<int, 2>
-convertInt2ToStdArray2(const int cArray[2]) {
-  return std::array<int, 2>{cArray[0], cArray[1]};
+static constexpr std::array<int, 2> convertInt2ToStdArray2(const int cArray[2])
+{
+	return std::array<int, 2>{cArray[0], cArray[1]};
 }
 
 static const std::vector<std::array<int, 2>>
-convertInt62ToVector6Array2(const int cArray[6][2]) {
-  return std::vector<std::array<int, 2>>{
-      convertInt2ToStdArray2(cArray[0]), convertInt2ToStdArray2(cArray[1]),
-      convertInt2ToStdArray2(cArray[2]), convertInt2ToStdArray2(cArray[3]),
-      convertInt2ToStdArray2(cArray[4]), convertInt2ToStdArray2(cArray[5]),
-  };
+convertInt62ToVector6Array2(const int cArray[6][2])
+{
+	return std::vector<std::array<int, 2>>{
+		convertInt2ToStdArray2(cArray[0]),
+		convertInt2ToStdArray2(cArray[1]),
+		convertInt2ToStdArray2(cArray[2]),
+		convertInt2ToStdArray2(cArray[3]),
+		convertInt2ToStdArray2(cArray[4]),
+		convertInt2ToStdArray2(cArray[5]),
+	};
 }
 
 extern "C" struct pokemon_detector_sv_context {
-  pokemon_detector_sv_context(const struct pokemon_detector_sv_config config)
-      : config(config), sceneDetector(config.classifier_lobby_my_select,
-                                      config.classifier_lobby_opponent_select,
-                                      config.classifier_black_transition,
-                                      config.classifier_select_my_team),
-        opponentPokemonCropper(
-            convertInt2ToStdArray2(config.opponent_col_range),
-            convertInt62ToVector6Array2(config.opponent_row_range)),
-        opponentPokemonIds(6),
-        selectionOrderCropper(
-            convertInt2ToStdArray2(config.selection_order_range_col),
-            convertInt62ToVector6Array2(config.selection_order_range_row)),
-        resultCropper(convertInt2ToStdArray2(config.result_range_col),
-                      std::vector<std::array<int, 2>>{
-                          convertInt2ToStdArray2(config.result_range_row[0])}),
-        resultRecognizer(config.result_n_bins, config.result_lose_max_index,
-                         config.result_lose_ratio, config.result_win_max_index,
-                         config.result_win_ratio) {}
+	pokemon_detector_sv_context(
+		const struct pokemon_detector_sv_config config)
+		: config(config),
+		  sceneDetector(config.classifier_lobby_my_select,
+				config.classifier_lobby_opponent_select,
+				config.classifier_black_transition,
+				config.classifier_select_my_team),
+		  opponentPokemonCropper(
+			  convertInt2ToStdArray2(config.opponent_col_range),
+			  convertInt62ToVector6Array2(
+				  config.opponent_row_range)),
+		  opponentPokemonIds(6),
+		  selectionOrderCropper(
+			  convertInt2ToStdArray2(
+				  config.selection_order_range_col),
+			  convertInt62ToVector6Array2(
+				  config.selection_order_range_row)),
+		  resultCropper(convertInt2ToStdArray2(config.result_range_col),
+				std::vector<std::array<int, 2>>{
+					convertInt2ToStdArray2(
+						config.result_range_row[0])}),
+		  resultRecognizer(config.result_n_bins,
+				   config.result_lose_max_index,
+				   config.result_lose_ratio,
+				   config.result_win_max_index,
+				   config.result_win_ratio)
+	{
+	}
 
-  const struct pokemon_detector_sv_config config;
+	const struct pokemon_detector_sv_config config;
 
-  cv::Mat screenBGRA, screenBGR, screenHSV;
+	cv::Mat screenBGRA, screenBGR, screenHSV;
 
-  SceneDetector sceneDetector;
+	SceneDetector sceneDetector;
 
-  EntityCropper opponentPokemonCropper;
-  PokemonRecognizer pokemonRecognizer;
-  std::vector<std::string> opponentPokemonIds;
+	EntityCropper opponentPokemonCropper;
+	PokemonRecognizer pokemonRecognizer;
+	std::vector<std::string> opponentPokemonIds;
 
-  EntityCropper selectionOrderCropper;
-  SelectionRecognizer selectionOrderRecognizer;
+	EntityCropper selectionOrderCropper;
+	SelectionRecognizer selectionOrderRecognizer;
 
-  EntityCropper resultCropper;
-  ResultRecognizer resultRecognizer;
+	EntityCropper resultCropper;
+	ResultRecognizer resultRecognizer;
 };
 
 extern "C" struct pokemon_detector_sv_context *
-pokemon_detector_sv_create(const struct pokemon_detector_sv_config config) {
-  return new pokemon_detector_sv_context(config);
+pokemon_detector_sv_create(const struct pokemon_detector_sv_config config)
+{
+	return new pokemon_detector_sv_context(config);
 }
 
 extern "C" void
-pokemon_detector_sv_destroy(struct pokemon_detector_sv_context *context) {
-  delete context;
+pokemon_detector_sv_destroy(struct pokemon_detector_sv_context *context)
+{
+	delete context;
 }
 
 extern "C" void
 pokemon_detector_sv_load_screen(struct pokemon_detector_sv_context *context,
-                                void *buf_bgra, int width, int height) {
-  context->screenBGRA = cv::Mat(height, width, CV_8UC4, buf_bgra);
+				void *buf_bgra, int width, int height)
+{
+	context->screenBGRA = cv::Mat(height, width, CV_8UC4, buf_bgra);
 }
 
 extern "C" enum pokemon_detector_sv_scene
-pokemon_detector_sv_detect_scene(struct pokemon_detector_sv_context *context) {
-  cv::Mat screenBGR, screenHSV;
-  cv::cvtColor(context->screenBGRA, screenBGR, cv::COLOR_BGRA2BGR);
-  cv::cvtColor(screenBGR, screenHSV, cv::COLOR_BGR2HSV);
-  return context->sceneDetector.detectScene(screenHSV);
+pokemon_detector_sv_detect_scene(struct pokemon_detector_sv_context *context)
+{
+	cv::Mat screenBGR, screenHSV;
+	cv::cvtColor(context->screenBGRA, screenBGR, cv::COLOR_BGRA2BGR);
+	cv::cvtColor(screenBGR, screenHSV, cv::COLOR_BGR2HSV);
+	return context->sceneDetector.detectScene(screenHSV);
 }
 
 extern "C" void pokemon_detector_sv_opponent_pokemon_crop(
-    struct pokemon_detector_sv_context *context) {
-  context->opponentPokemonCropper.crop(context->screenBGRA);
-  context->opponentPokemonCropper.generateMask();
+	struct pokemon_detector_sv_context *context)
+{
+	context->opponentPokemonCropper.crop(context->screenBGRA);
+	context->opponentPokemonCropper.generateMask();
 }
 
 const char PATH_SEPARATOR = '/';
 
 extern "C" void pokemon_detector_sv_opponent_pokemon_export_image(
-    struct pokemon_detector_sv_context *context, int index, const char *basedir,
-    const char *filename) {
-  std::string b(basedir), f(filename), filepath = b + PATH_SEPARATOR + f;
-  cv::imwrite(filepath.c_str(),
-              context->opponentPokemonCropper.imagesBGRA[index]);
+	struct pokemon_detector_sv_context *context, int index,
+	const char *filepath)
+{
+	std::filesystem::path path(filepath);
+	std::filesystem::create_directories(path.parent_path());
+
+	cv::imwrite(filepath,
+		    context->opponentPokemonCropper.imagesBGRA[index]);
 }
 
 extern "C" const char *pokemon_detector_sv_opponent_pokemon_recognize(
-    struct pokemon_detector_sv_context *context, int index) {
-  auto id = context->pokemonRecognizer.recognizePokemon(
-      context->opponentPokemonCropper.imagesBGR[index],
-      context->opponentPokemonCropper.masks[index]);
-  context->opponentPokemonIds[index] = id;
-  return context->opponentPokemonIds[index].c_str();
+	struct pokemon_detector_sv_context *context, int index)
+{
+	auto id = context->pokemonRecognizer.recognizePokemon(
+		context->opponentPokemonCropper.imagesBGR[index],
+		context->opponentPokemonCropper.masks[index]);
+	context->opponentPokemonIds[index] = id;
+	return context->opponentPokemonIds[index].c_str();
 }
 
 extern "C" void pokemon_detector_sv_my_selection_order_crop(
-    struct pokemon_detector_sv_context *context) {
-  context->selectionOrderCropper.crop(context->screenBGRA);
+	struct pokemon_detector_sv_context *context)
+{
+	context->selectionOrderCropper.crop(context->screenBGRA);
 }
 
 extern "C" int pokemon_detector_sv_my_selection_order_recognize(
-    struct pokemon_detector_sv_context *context, int index) {
-  return context->selectionOrderRecognizer.recognizeSelection(
-      context->selectionOrderCropper.imagesBGR[index]);
+	struct pokemon_detector_sv_context *context, int index)
+{
+	return context->selectionOrderRecognizer.recognizeSelection(
+		context->selectionOrderCropper.imagesBGR[index]);
 }
 
 extern "C" void pokemon_detector_sv_my_selection_order_export_image(
-    struct pokemon_detector_sv_context *context, int index, const char *path,
-    bool shouldBeBlank) {
-  cv::Mat image = context->selectionOrderCropper.imagesBGRA[index];
-  if (shouldBeBlank) {
-    image *= 0;
-  }
-  cv::imwrite(path, image);
+	struct pokemon_detector_sv_context *context, int index,
+	const char *filepath, bool shouldBeBlank)
+{
+	std::filesystem::path path(filepath);
+	std::filesystem::create_directories(path.parent_path());
+
+	cv::Mat image = context->selectionOrderCropper.imagesBGRA[index];
+	if (shouldBeBlank) {
+		image *= 0;
+	}
+	cv::imwrite(filepath, image);
 }
 
 extern "C" void
-pokemon_detector_sv_result_crop(struct pokemon_detector_sv_context *context) {
-  context->resultCropper.crop(context->screenBGRA);
+pokemon_detector_sv_result_crop(struct pokemon_detector_sv_context *context)
+{
+	context->resultCropper.crop(context->screenBGRA);
 }
 
 extern "C" enum pokemon_detector_sv_result pokemon_detector_sv_result_recognize(
-    struct pokemon_detector_sv_context *context) {
-  return context->resultRecognizer.recognizeResult(
-      context->resultCropper.imagesBGR[0]);
+	struct pokemon_detector_sv_context *context)
+{
+	return context->resultRecognizer.recognizeResult(
+		context->resultCropper.imagesBGR[0]);
 }
 
 extern "C" void pokemon_detector_sv_matchstate_append(
-    struct pokemon_detector_sv_matchstate *matchstate, const char *filepath) {
-  std::ofstream ofs(filepath, std::ios_base::app);
-  if (matchstate->result == POKEMON_DETECTOR_SV_RESULT_UNKNOWN) {
-    ofs << "UNKNOWN";
-  } else if (matchstate->result == POKEMON_DETECTOR_SV_RESULT_LOSE) {
-    ofs << "LOSE";
-  } else if (matchstate->result == POKEMON_DETECTOR_SV_RESULT_WIN) {
-    ofs << "WIN";
-  }
-  ofs << "\t";
-  for (int i = 0; i < 6; i++) {
-    ofs << matchstate->my_selection_order[i] << "\t";
-  }
-  for (int i = 0; i < 6; i++) {
-    ofs << matchstate->opponent_pokemon_ids[i] << "\t";
-  }
-  ofs << "\n";
+	struct pokemon_detector_sv_matchstate *matchstate, const char *filepath)
+{
+	std::filesystem::path path(filepath);
+	std::filesystem::create_directories(path.parent_path());
+	std::ofstream ofs(filepath, std::ios_base::app);
+	if (matchstate->result == POKEMON_DETECTOR_SV_RESULT_UNKNOWN) {
+		ofs << "UNKNOWN";
+	} else if (matchstate->result == POKEMON_DETECTOR_SV_RESULT_LOSE) {
+		ofs << "LOSE";
+	} else if (matchstate->result == POKEMON_DETECTOR_SV_RESULT_WIN) {
+		ofs << "WIN";
+	}
+	ofs << "\t";
+	for (int i = 0; i < 6; i++) {
+		ofs << matchstate->my_selection_order[i] << "\t";
+	}
+	for (int i = 0; i < 6; i++) {
+		ofs << matchstate->opponent_pokemon_ids[i] << "\t";
+	}
+	ofs << "\n";
 }
--- pokemon-detector-sv.h
+++ pokemon-detector-sv.h
@@ -5,91 +5,94 @@ extern "C" {
 #endif
 
 enum pokemon_detector_sv_scene {
-  POKEMON_DETECTOR_SV_SCENE_UNDEFINED,
-  POKEMON_DETECTOR_SV_SCENE_SELECT_POKEMON,
-  POKEMON_DETECTOR_SV_SCENE_BLACK_TRANSITION
+	POKEMON_DETECTOR_SV_SCENE_UNDEFINED,
+	POKEMON_DETECTOR_SV_SCENE_SELECT_POKEMON,
+	POKEMON_DETECTOR_SV_SCENE_BLACK_TRANSITION
 };
 
 enum pokemon_detector_sv_result {
-  POKEMON_DETECTOR_SV_RESULT_UNKNOWN,
-  POKEMON_DETECTOR_SV_RESULT_LOSE,
-  POKEMON_DETECTOR_SV_RESULT_WIN
+	POKEMON_DETECTOR_SV_RESULT_UNKNOWN,
+	POKEMON_DETECTOR_SV_RESULT_LOSE,
+	POKEMON_DETECTOR_SV_RESULT_WIN
 };
 
 struct pokemon_detector_sv_hist_classifier {
-  const int range_col[2];
-  const int range_row[2];
-  const int hist_channel;
-  const int hist_bins;
-  const int hist_max_index;
-  const double hist_ratio;
+	const int range_col[2];
+	const int range_row[2];
+	const int hist_channel;
+	const int hist_bins;
+	const int hist_max_index;
+	const double hist_ratio;
 };
 
 struct pokemon_detector_sv_config {
-  const int opponent_col_range[2];
-  const int opponent_row_range[6][2];
-  const int selection_order_range_col[2];
-  const int selection_order_range_row[6][2];
-  const struct pokemon_detector_sv_hist_classifier classifier_lobby_my_select;
-  const struct pokemon_detector_sv_hist_classifier
-      classifier_lobby_opponent_select;
-  const struct pokemon_detector_sv_hist_classifier classifier_black_transition;
-  const struct pokemon_detector_sv_hist_classifier classifier_select_my_team;
+	const int opponent_col_range[2];
+	const int opponent_row_range[6][2];
+	const int selection_order_range_col[2];
+	const int selection_order_range_row[6][2];
+	const struct pokemon_detector_sv_hist_classifier
+		classifier_lobby_my_select;
+	const struct pokemon_detector_sv_hist_classifier
+		classifier_lobby_opponent_select;
+	const struct pokemon_detector_sv_hist_classifier
+		classifier_black_transition;
+	const struct pokemon_detector_sv_hist_classifier
+		classifier_select_my_team;
 
-  const int result_range_col[2];
-  const int result_range_row[1][2];
-  const int result_n_bins;
-  const int result_lose_max_index;
-  const double result_lose_ratio;
-  const int result_win_max_index;
-  const double result_win_ratio;
+	const int result_range_col[2];
+	const int result_range_row[1][2];
+	const int result_n_bins;
+	const int result_lose_max_index;
+	const double result_lose_ratio;
+	const int result_win_max_index;
+	const double result_win_ratio;
 };
 
 const struct pokemon_detector_sv_config pokemon_detector_sv_default_config = {
-    .opponent_col_range = {1239, 1337},
-    .opponent_row_range = {{228, 326},
-                           {330, 428},
-                           {432, 530},
-                           {534, 632},
-                           {636, 734},
-                           {738, 836}},
-    .selection_order_range_col = {795, 827},
-    .selection_order_range_row = {{154, 186},
-                                  {271, 303},
-                                  {388, 420},
-                                  {503, 535},
-                                  {619, 651},
-                                  {735, 767}},
-    .classifier_lobby_my_select = {.range_col = {149, 811},
-                                   .range_row = {139, 842},
-                                   .hist_channel = 0,
-                                   .hist_bins = 30,
-                                   .hist_max_index = 17,
-                                   .hist_ratio = 0.5},
-    .classifier_lobby_opponent_select = {.range_col = {1229, 1649},
-                                         .range_row = {227, 836},
-                                         .hist_channel = 0,
-                                         .hist_bins = 30,
-                                         .hist_max_index = 0,
-                                         .hist_ratio = 0.8},
-    .classifier_black_transition = {.range_col = {400, 600},
-                                    .range_row = {400, 600},
-                                    .hist_channel = 2,
-                                    .hist_bins = 8,
-                                    .hist_max_index = 0,
-                                    .hist_ratio = 0.8},
-    .result_range_col = {483, 697},
-    .result_range_row = {{958, 1039}},
-    .result_n_bins = 16,
-    .result_lose_max_index = 11,
-    .result_lose_ratio = 0.3,
-    .result_win_max_index = 2,
-    .result_win_ratio = 0.3};
+	.opponent_col_range = {1239, 1337},
+	.opponent_row_range = {{228, 326},
+			       {330, 428},
+			       {432, 530},
+			       {534, 632},
+			       {636, 734},
+			       {738, 836}},
+	.selection_order_range_col = {795, 827},
+	.selection_order_range_row = {{154, 186},
+				      {271, 303},
+				      {388, 420},
+				      {503, 535},
+				      {619, 651},
+				      {735, 767}},
+	.classifier_lobby_my_select = {.range_col = {149, 811},
+				       .range_row = {139, 842},
+				       .hist_channel = 0,
+				       .hist_bins = 30,
+				       .hist_max_index = 17,
+				       .hist_ratio = 0.5},
+	.classifier_lobby_opponent_select = {.range_col = {1229, 1649},
+					     .range_row = {227, 836},
+					     .hist_channel = 0,
+					     .hist_bins = 30,
+					     .hist_max_index = 0,
+					     .hist_ratio = 0.8},
+	.classifier_black_transition = {.range_col = {400, 600},
+					.range_row = {400, 600},
+					.hist_channel = 2,
+					.hist_bins = 8,
+					.hist_max_index = 0,
+					.hist_ratio = 0.8},
+	.result_range_col = {483, 697},
+	.result_range_row = {{958, 1039}},
+	.result_n_bins = 16,
+	.result_lose_max_index = 11,
+	.result_lose_ratio = 0.3,
+	.result_win_max_index = 2,
+	.result_win_ratio = 0.3};
 
 struct pokemon_detector_sv_matchstate {
-  enum pokemon_detector_sv_result result;
-  int my_selection_order[6];
-  const char *opponent_pokemon_ids[6];
+	enum pokemon_detector_sv_result result;
+	int my_selection_order[6];
+	const char *opponent_pokemon_ids[6];
 };
 
 struct pokemon_detector_sv_context;
@@ -99,41 +102,41 @@ pokemon_detector_sv_create(const struct pokemon_detector_sv_config config);
 
 void pokemon_detector_sv_destroy(struct pokemon_detector_sv_context *context);
 
-void pokemon_detector_sv_load_screen(
-    struct pokemon_detector_sv_context *context, void *buf_bgra, int width,
-    int height);
+void pokemon_detector_sv_load_screen(struct pokemon_detector_sv_context *context,
+				     void *buf_bgra, int width, int height);
 
 enum pokemon_detector_sv_scene
 pokemon_detector_sv_detect_scene(struct pokemon_detector_sv_context *context);
 
 void pokemon_detector_sv_opponent_pokemon_crop(
-    struct pokemon_detector_sv_context *context);
+	struct pokemon_detector_sv_context *context);
 
 void pokemon_detector_sv_opponent_pokemon_export_image(
-    struct pokemon_detector_sv_context *context, int index, const char *basedir,
-    const char *filename);
+	struct pokemon_detector_sv_context *context, int index,
+	const char *filepath);
 
 const char *pokemon_detector_sv_opponent_pokemon_recognize(
-    struct pokemon_detector_sv_context *context, int index);
+	struct pokemon_detector_sv_context *context, int index);
 
 int pokemon_detector_sv_recognize_selection_order(
-    struct pokemon_detector_sv_context *context, int index);
+	struct pokemon_detector_sv_context *context, int index);
 
 void pokemon_detector_sv_my_selection_order_crop(
-    struct pokemon_detector_sv_context *context);
+	struct pokemon_detector_sv_context *context);
 int pokemon_detector_sv_my_selection_order_recognize(
-    struct pokemon_detector_sv_context *context, int index);
+	struct pokemon_detector_sv_context *context, int index);
 void pokemon_detector_sv_my_selection_order_export_image(
-    struct pokemon_detector_sv_context *context, int index, const char *path,
-    bool shouldBeBlank);
+	struct pokemon_detector_sv_context *context, int index,
+	const char *filepath, bool shouldBeBlank);
 
 void pokemon_detector_sv_result_crop(
-    struct pokemon_detector_sv_context *context);
+	struct pokemon_detector_sv_context *context);
 enum pokemon_detector_sv_result pokemon_detector_sv_result_recognize(
-    struct pokemon_detector_sv_context *context);
+	struct pokemon_detector_sv_context *context);
 
 void pokemon_detector_sv_matchstate_append(
-    struct pokemon_detector_sv_matchstate *matchstate, const char *filepath);
+	struct pokemon_detector_sv_matchstate *matchstate,
+	const char *filepath);
 
 #ifdef __cplusplus
 }
